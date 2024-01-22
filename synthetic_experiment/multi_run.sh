8af81d2f7ad143e163e068b8af8aa576d3cb6715
Elchin <elchin.jafarov@gmail.com>
2024-01-22 18:56:22 -0500

    added 50/90variance

---
+++ synthetic_experiment/multi_run.sh
@@ -0,0 +1,4 @@
+julia AC-MADS-TEM.jl config-cmax-gpp-md1.yaml            >> logfile-cmax.log    2>&1 &
+julia AC-MADS-TEM.jl config-nmax-krb-npp-cfall-md1.yaml  >> logfile-npp-krb.log 2>&1 &
+julia AC-MADS-TEM.jl config-cfall-npp-vegc-md1.yaml      >> logfile-cfall.log   2>&1 &
+julia AC-MADS-TEM.jl config-nfall-npp-vegc-vegn-md1.yaml >> logfile-nfall.log   2>&1 &
